2a8fe7077c80cc85e9dd2ba75c631bb33653dbcd
kimjw <kimjw@kimjw>
2023-12-12 18:07:07 +0900

    수정 조금

--- src/main/java/com/market/evo/product/ProductMapper.java
+++ src/main/java/com/market/evo/product/ProductMapper.java
@@ -3,10 +3,12 @@ package com.market.evo.product;
 import java.util.List;
 import java.util.Map;
 
+
 public interface ProductMapper {
 
 	public List<Map<String, Object>> categoryList();
 	public List<Map<String, Object>> detailCateList(int categorycode);
 	public abstract int addProduct(Product p);
 	public List<Map<String, String>> idViewProduct(String p_m_id);
+	public abstract List<Product> viewProducts(Product p);
 }

7b31f0c32a62cb5299fc23dd65fd8786309d036b
zyu22 <zyu22@naver.com>
2023-12-15 17:45:24 +0900

    상품추가 사이트 새로 만듬 / 상품추가기능 개발 진행중 231215 지유림

--- src/main/java/com/market/evo/product/ProductMapper.java
+++ src/main/java/com/market/evo/product/ProductMapper.java
@@ -1,10 +1,7 @@
 package com.market.evo.product;
 
-import java.math.BigDecimal;
 import java.util.List;
 import java.util.Map;
-
-import org.apache.ibatis.annotations.Param;
 
 public interface ProductMapper {
 
@@ -12,7 +9,7 @@ public interface ProductMapper {
 	public List<Map<String, Object>> detailCateList(int categorycode);
 	public abstract int addProduct(Product p);
 	public List<Map<String, String>> idViewProduct(String p_m_id);
-		public abstract int addImage(List<Map<String, String>> imgList);
+	public abstract int addImage(List<Map<String, Object>> imgList);
 	public abstract int getProNo(Product p);
 	public List<Map<String, Object>> getProductsUpperWearWithImage(String categoryName);
-	}
+}

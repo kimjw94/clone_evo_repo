c986d601a945aa563f58b388955f82d287b0bb52
zyu22 <zyu22@naver.com>
2023-12-18 12:14:07 +0900

    상품추가 기능 구현 231218 지유림

--- src/main/java/com/market/evo/product/ProductMapper.java
+++ src/main/java/com/market/evo/product/ProductMapper.java
@@ -3,14 +3,17 @@ package com.market.evo.product;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.ibatis.annotations.Param;
+
 public interface ProductMapper {
 
 	public List<Map<String, Object>> categoryList();
 	public List<Map<String, Object>> detailCateList(int categorycode);
 	public abstract int addProduct(Product p);
 	public List<Map<String, String>> idViewProduct(String p_m_id);
-	public abstract int addImage(List<Map<String, Object>> imgList);
+	public abstract int addImage(Map<String, Object> imgList);
 	public abstract int getProNo(Product p);
+	public abstract int addInventory(Map<String, Object> invenList);
 	public List<Map<String, Object>> getProductsWithImagebyCategory(String categoryName);
 	public List<Map<String, Object>> getProductswithImagebyDetailCategory(String categoryDetailName);
 }
